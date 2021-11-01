2b1575307ec19e0dc2cfc42ac23e3386a721f917
Automation_13_1 <sachindeokar351@gmail.com>
2021-11-01 08:45:08 +0530

    java

---
+++ OOPS/Square.java
@@ -0,0 +1,18 @@
+package NARAYAN;
+
+public class Square {
+     static double Area(int a, int b){
+    	 
+    	 return a*b;
+    	 
+    
+     }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("area of square is="+Square.Area(10,10));
+         System.out.println("area of square is="+Square.Area(100,200));
+         System.out.println("area of square is="+Square.Area(50,100));
+         System.out.println("area of square is="+Square.Area(150,200));
+	}
+
+}
